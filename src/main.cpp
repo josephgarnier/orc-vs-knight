6a9c320f6b663952519a86362f21a29f90c4f916
Joseph Garnier <garnjose@gmail.com>
2021-09-05 12:44:11 +0200

    Setting up the project

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,9 @@
 * LICENSE file in the root directory of this source tree.
 ******************************************************************************/
 
-#include "project-name/include1.h"
-#include "project-name/include2.h"
-#include "source1.h"
-#include "sub1/sub1.h"
-#include "sub2/sub2.h"
-#include "project-name/project-name_pch.h"
+#include "iostream"
 
 int main() {
 	std::cout << "Main: Hello, World!" << std::endl;
-	Include1 include1;
-	Include2 include2;
-	Source1 source1;
-	Sub1 sub1;
-	Sub2 sub2;
 	return 0;
 }
